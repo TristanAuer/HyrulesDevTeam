105aa3fe65315b8d6edfb2c4c9126f80dfa6d8cd
Samuel <codesamro@gmail.com>
2021-10-12 20:12:28 -0400

    Update Update

--- LibraryHyrule/Class1.cs
+++ LibraryHyrule/Class1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static LibraryHyrule.ProgramUI;
 
 namespace LibraryHyrule
 {
@@ -10,21 +11,80 @@ namespace LibraryHyrule
     {
         //Zelda inspired text based adventure game
         //Introduction
-        
+        Dictionary<string, Room> Rooms = new Dictionary<string, Room>
+        {
+            {"start", start },
+            {"left", left },
+            {"right", right },
+            {"center", center },
+            {"straight", center },
+            {"up", up },
+            {"down", down },
+        };
+        public List<Item> Inventory = new List<Item>();
         public void Run()
         {
+
             Console.WriteLine("What is you're name?");
             string playerName = Console.ReadLine();
             Console.WriteLine("You wake up in a cave, surprisingly bright; confused to everything you start to explore. One thing you are sure about is you're" +
                 "name,\n" +
                 $"{playerName}");
+            Console.ReadKey();
             
             bool alive = true;
             while (alive)
             {
+                Room currentRoom = start;
                 string command = Console.ReadLine().ToLower();
                 Console.Clear();
-                
+                if (command.StartsWith("go ") || command.StartsWith("exit"))
+                {
+                    foreach (string exit in currentRoom.Exits)
+                    {
+                        if (command.Contains(exit) &&
+                           Rooms.ContainsKey(exit))
+                        {
+                            currentRoom = Rooms[exit];
+                            foundExit = true;
+                            break;
+                        }
+                    }
+                    if (!foundExit)
+                    {
+                        Console.WriteLine("Uh... Go where?");
+                    }
+
+                }
+                else if (command.StartsWith("get ") || command.StartsWith("take") || command.StartsWith("grab"))
+                {
+                    bool foundItem = false;
+                    foreach (Item item in currentRoom.Items)
+                    {   
+
+                        if (!foundItem && command.Contains(item.ToString()))
+                            {  
+
+                            Console.WriteLine($"you found a {item}." + "[ress any key to continue...");
+                            
+                            currentRoom.RemoveItem(item);
+                            inventory.Add(item);
+                            foundItem = true;
+                            Console.ReadKey();
+                            
+                            }
+                    }
+                    Console.WriteLine("I don't know what you're talking about.");
+                }
+                else if (command.StartsWith("use ") || command.StartsWith("activate"))
+                {
+                    Console.WriteLine("I doubt you know how.");
+                }
+                else
+                {
+                    Console.WriteLine("*BUUUUUURP What?");
+                }
+
             }
 
         }
---
+++ LibraryHyrule/ProgramUI.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryHyrule
+{
+    class ProgramUI
+    {
+        Dictionary<string, Room> Rooms = new Dictionary<string, Room>
+        {
+            {"start", start },
+            {"left", left },
+            {"right", right },
+            {"center", center },
+            {"straight", center },
+            {"up", up },
+            {"down", down },
+        };
+        public enum Item { sword, bow, spear };
+        public List<Item> inventory = new List<Item>();
+
+        public static Room start = new Room("All the way back here huh.\n\n" +
+            "Once again you can GO LEFT, RIGHT,and CENTER\n",
+            new List<string> { "left", "right", "center" });
+        public static Room left = new Room("All the way back here huh.\n\n" +
+            "You can GO LEFT, RIGHT,and CENTER\n",
+            new List<string> { "up", "down", "start" });
+        public static Room right = new Room("All the way back here huh.\n\n" +
+            "You can GO LEFT, RIGHT,and CENTER\n",
+            new List<string> { "start" },
+            new List<Item> { Item.spear});
+        public static Room center = new Room("All the way back here huh.\n\n" +
+            "You can GO LEFT, RIGHT,and CENTER\n",
+            new List<string> { "start" });
+        public static Room up = new Room("All the way back here huh.\n\n" +
+            "You can GO LEFT, RIGHT,and CENTER\n",
+            new List<string> { "start" },
+            new List<Item> { Item.bow });
+        public static Room down = new Room("All the way back here huh.\n\n" +
+            "You can GO LEFT, RIGHT,and CENTER\n",
+            new List<string> { "start" },
+            new List<Item> {Item.sword });
+
+
+        public class Room
+        {
+            public List<Item> Items { get; }
+            public void RemoveItem(Item item)
+            {
+                if (Items.Contains(item))
+                {
+                    Items.Remove(item);
+                }
+            }
+            public string Splash { get; }
+            public List<string> Exits { get; }
+            public Room(string splash, List<string> exits, List<Item> items)
+            {
+                Splash = splash;
+                Exits = exits;
+                Items = items;
+            }
+        }
+
+        public class Weapons
+        {
+            public List<string> Weapon { get; }
+            public Weapons(List<string> weapons)
+            {
+                Weapon = weapons;
+            }
+        }
+    }
+}
